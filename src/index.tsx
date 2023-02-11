491de21c7d4500caccdc3d28f0771fccbca1795b
Jacob Parker <jacobparker1992@gmail.com>
2023-02-11 13:55:49 +0000

    Fix types

--- src/index.tsx
+++ src/index.tsx
@@ -23,34 +23,53 @@ export type Ref = {
   scrollIntoView: (options?: ScrollIntoViewOptions) => void;
 };
 
-const create = (Component: typeof ScrollIntoViewNativeComponent) =>
-  React.forwardRef<Ref | undefined, NativeProps>((props, ref) => {
-    const nativeComponent = React.useRef<any>();
-    React.useImperativeHandle(
-      ref,
-      (): Ref => ({
-        scrollIntoView({ behavior = 'smooth', insets = {} } = {}) {
-          const { top = 0, left = 0, bottom = 0, right = 0 } = insets;
-          const animated = behavior === 'smooth';
-          Commands.scrollIntoView(
-            nativeComponent.current,
-            top,
-            left,
-            bottom,
-            right,
-            animated
-          );
-        },
-      }),
-      []
+const useNativeMethods = (ref: React.Ref<Ref | undefined>) => {
+  const nativeComponent = React.useRef<any>();
+  React.useImperativeHandle(
+    ref,
+    (): Ref => ({
+      scrollIntoView({ behavior = 'smooth', insets = {} } = {}) {
+        const { top = 0, left = 0, bottom = 0, right = 0 } = insets;
+        const animated = behavior === 'smooth';
+        Commands.scrollIntoView(
+          nativeComponent.current,
+          top,
+          left,
+          bottom,
+          right,
+          animated
+        );
+      },
+    }),
+    []
+  );
+
+  return nativeComponent;
+};
+
+export default React.forwardRef<Ref | undefined, NativeProps>((props, ref) => {
+  const nativeComponent = useNativeMethods(ref);
+
+  return <ScrollIntoViewNativeComponent ref={nativeComponent} {...props} />;
+});
+
+let AnimatedScrollIntoViewNativeComponent:
+  | Animated.AnimatedComponent<typeof ScrollIntoViewNativeComponent>
+  | undefined;
+
+export const AnimatedComponent = React.forwardRef<
+  Ref | undefined,
+  Animated.AnimatedProps<NativeProps>
+>((props, ref) => {
+  const nativeComponent = useNativeMethods(ref);
+
+  if (AnimatedScrollIntoViewNativeComponent == null) {
+    AnimatedScrollIntoViewNativeComponent = Animated.createAnimatedComponent(
+      ScrollIntoViewNativeComponent
     );
+  }
 
-    return <Component ref={nativeComponent} {...props} />;
-  });
-
-export default create(ScrollIntoViewNativeComponent);
-
-export const AnimatedComponent = create(
-  // @ts-expect-error
-  Animated.createAnimatedComponent(ScrollIntoViewNativeComponent)
-);
+  return (
+    <AnimatedScrollIntoViewNativeComponent ref={nativeComponent} {...props} />
+  );
+});
